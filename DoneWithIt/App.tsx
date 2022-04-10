31dcb1366a2e05c903bfa3b785b9f4dfc21ca404
mdsflyboy <mdsflyboy@gmail.com>
2022-04-09 22:00:51 -0700

    working on App.tsx

--- DoneWithIt/App.tsx
+++ DoneWithIt/App.tsx
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
+  console.log("App executed");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={console.log("Test")}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit commodi nisi incidunt? Explicabo deleniti harum provident ipsam natus, totam quod officiis expedita quas asperiores eius voluptas quo. Expedita, earum quis.
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'blue',
+    height: "100%",
+    width: "100%",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
